8b69c6cf33c248c44a06f20dc3752c68854c3678
Keviweto-Yhoshu <89389070+Keviweto-Yhoshu@users.noreply.github.com>
2024-05-10 16:40:29 +0530

    Add files via upload

---
+++ car.js
@@ -0,0 +1,10 @@
+AFRAME.registerComponent('car', {
+    schema: {
+      message: {type: 'string', default: 'Hello, World!'}
+    },
+    init: function () {
+        console.log(this.data.message);
+      }
+  });
+
+ 
